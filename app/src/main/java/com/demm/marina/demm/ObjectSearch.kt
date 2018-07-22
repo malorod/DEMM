437e8cc167dda2bd87aa215a7e199b75ebddf39e
Marina <diadora22*gh>
2018-07-22 11:55:14 +0200

    Primeras 3 vistas

---
+++ app/src/main/java/com/demm/marina/demm/ObjectSearch.kt
@@ -0,0 +1,12 @@
+package com.demm.marina.demm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ObjectSearch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_object_search)
+    }
+}
