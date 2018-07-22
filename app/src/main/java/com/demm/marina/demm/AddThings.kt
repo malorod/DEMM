548b9d43dc08338cd578c6f8a8ad20e885464587
Marina <diadora22*gh>
2018-07-22 12:47:48 +0200

    Renombrar Object. Enlazar botón y función añadir.

--- app/src/main/java/com/demm/marina/demm/AddThings.kt
+++ app/src/main/java/com/demm/marina/demm/AddThings.kt
@@ -3,7 +3,7 @@ package com.demm.marina.demm
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class AddObject : AppCompatActivity() {
+class AddThings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/demm/marina/demm/MainActivity.kt
+++ app/src/main/java/com/demm/marina/demm/MainActivity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add.setOnClickListener{
+
+            val starAddThings = Intent (this, AddThings::class.java)
+            startActivity(starAddThings)
+        }
 
         findViewById(R.id.search).setOnClickListener {
 
