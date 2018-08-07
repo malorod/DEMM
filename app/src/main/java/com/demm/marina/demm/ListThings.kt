a717046e5c567c243073c5658a2689dfa56b3ede
malorod <maipsilon@gmail.com>
2018-08-07 23:16:40 +0200

    Borrando mierda

--- app/src/main/java/com/demm/marina/demm/ListThings.kt
+++ app/src/main/java/com/demm/marina/demm/ListThings.kt
@@ -3,7 +3,6 @@ package com.demm.marina.demm
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_list_things.*
 
 class ListThings : AppCompatActivity() {
@@ -17,25 +16,7 @@ class ListThings : AppCompatActivity() {
             startActivity(starAddThings)
         }
 
-        /*val thingRepository: ThingRepository = (application as DEMMApplication).repository
+        val thingRepository: ThingRepository = (application as DEMMApplication).repository
 
-        var listView = thingList
-
-        var placement="Estante"
-
-        val thingList: MutableList<Thing> = thingRepository.getByPlacement(placement)!!
-
-        val listItems = arrayOfNulls<String>(thingList.size)
-
-        for (i in 0 until thingList.size) {
-            val thing = thingList.get(i)
-            listItems[i] = thing.name
-        }
-
-        val adapter = ArrayAdapter(this, android.R.layout.activity_list_item, listItems)
-        listView.adapter = adapter*/
-
-        /*val adapter = ThingAdapter(this, thingRepository)
-        listView.adapter = adapter*/
     }
 }
