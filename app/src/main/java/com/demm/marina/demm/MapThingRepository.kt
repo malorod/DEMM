9341fc558da456755343b2a05086af6cdc6c1f75
malorod <maipsilon@gmail.com>
2018-08-11 00:44:27 +0200

    Cambiar los for

--- app/src/main/java/com/demm/marina/demm/MapThingRepository.kt
+++ app/src/main/java/com/demm/marina/demm/MapThingRepository.kt
@@ -4,11 +4,11 @@ class MapThingRepository : ThingRepository {
     var map = mutableMapOf<String, MutableList<Thing>>()
 
     override fun save(thing: Thing) {
-        if (map.containsKey(thing.placement)) {
+        if (this.map.containsKey(thing.placement)) {
             val thingsList = map.get(thing.placement)
             thingsList!!.add(thing)
         } else {
-            map.put(thing.placement, mutableListOf<Thing>(thing))
+            this.map.put(thing.placement, mutableListOf<Thing>(thing))
         }
     }
 
@@ -31,20 +31,16 @@ class MapThingRepository : ThingRepository {
 
     override fun getAllPlacement(thingName: String): MutableList<String> {
 
-        val keys = map.keys
+        val keys = map.values
         val placements = mutableListOf<String>()
 
-        for (i in 0 until keys.size){
-
-            val listThings = map.get(keys.elementAt(i))
-
-            for (j in 0 until listThings!!.size){
-
-                if (listThings[j].name == thingName){
-                    placements.add(listThings[j].placement)
-                }
+        keys.forEach { thingsList ->
+            thingsList.forEach {
+                if ((it.name == thingName) && !(placements.contains(it.placement)))
+                    placements.add(it.placement)
             }
         }
+
         return placements
     }
 }
