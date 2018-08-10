5307a7397a5f781934f69c5359b64d27692b74f7
malorod <maipsilon@gmail.com>
2018-08-10 22:25:04 +0200

    La vista listar lugares muestra el nombre de la Cosa de la que se buscan los Lugares en los que se encuentra

--- app/src/main/java/com/demm/marina/demm/ListPlacements.kt
+++ app/src/main/java/com/demm/marina/demm/ListPlacements.kt
@@ -2,11 +2,29 @@ package com.demm.marina.demm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class ListPlacements : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_placements)
+
+        val thingRepository: ThingRepository = (application as DEMMApplication).repository
+        val bundle = intent.extras
+        val thing = bundle.getString("thing")
+
+        val thingTextView = findViewById(R.id.thingSearch) as TextView
+        thingTextView.text = getString(R.string.textListPlacement, thing)
+        thingTextView.text = getString(R.string.textListPlacement, thing)
+
+        val listView : ListView = findViewById(R.id.listPlacement) as ListView
+        val listPlacements = thingRepository.getAllPlacement(thing)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listPlacements)
+
+        listView.adapter = adapter
     }
 }
