183f9e0fc86461e7efa60a10857dfeeba3269cb5
malorod <maipsilon@gmail.com>
2018-08-10 22:29:03 +0200

    El botón buscar enlaza con la vista listar lugares enviando la cosa buscada

--- app/src/main/java/com/demm/marina/demm/SearchThings.kt
+++ app/src/main/java/com/demm/marina/demm/SearchThings.kt
@@ -1,12 +1,31 @@
 package com.demm.marina.demm
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_object_search.*
 
 class SearchThings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_object_search)
+
+        searchThing.setOnClickListener{
+
+            if (!(thingName.text.isNullOrEmpty())){
+                val listPlacements = Intent(this, ListPlacements::class.java)
+
+                val thing = Bundle()
+                thing.putString(
+                        "thing",
+                        thingName.text.toString()
+                )
+                listPlacements.putExtras(thing)
+
+                startActivity(listPlacements)
+                finish()
+            }
+        }
     }
 }
