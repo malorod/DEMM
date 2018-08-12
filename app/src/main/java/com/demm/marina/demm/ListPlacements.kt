8408f486006c973b3cef0c0e7ec801bae9d44d86
malorod <maipsilon@gmail.com>
2018-08-12 20:21:10 +0200

    Cada elemento de la lista de Lugares al ser pulsado muestra todas las Cosas que contine

--- app/src/main/java/com/demm/marina/demm/ListPlacements.kt
+++ app/src/main/java/com/demm/marina/demm/ListPlacements.kt
@@ -1,7 +1,9 @@
 package com.demm.marina.demm
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
@@ -25,5 +27,20 @@ class ListPlacements : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listPlacements)
 
         listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener{ parent, view, position, id ->
+
+            val selectedThing = parent.getItemAtPosition(position) as String
+            val startListThings = Intent(this, ListThings::class.java)
+
+            val thingBundle = Bundle()
+            thingBundle.putString("name", thing)
+            thingBundle.putString("placement", selectedThing)
+            startListThings.putExtras(thingBundle)
+
+            startActivity(startListThings)
+            finish()
+
+        }
     }
 }
