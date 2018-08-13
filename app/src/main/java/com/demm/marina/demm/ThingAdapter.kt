f293bd46924b7335fa532a88196fb07e2a9e7982
malorod <maipsilon@gmail.com>
2018-08-13 09:10:08 +0200

    Modificar código para que siga ViewHolder Patter

--- app/src/main/java/com/demm/marina/demm/ThingAdapter.kt
+++ app/src/main/java/com/demm/marina/demm/ThingAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.TextView
 
 class ThingAdapter(private val context: Context, private val thingsList: MutableList<Thing>): BaseAdapter(){
@@ -25,10 +26,36 @@ class ThingAdapter(private val context: Context, private val thingsList: Mutable
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val rowView = inflater.inflate(R.layout.list_item_thing, parent, false)
-        val titleTextView = rowView.findViewById(R.id.thingName) as TextView
         val thing = getItem(position) as Thing
-        titleTextView.text = thing.name
-        return rowView
+
+        val view: View
+        val holder: ViewHolder
+
+        if (convertView == null){
+
+            view = inflater.inflate(R.layout.list_item_thing, parent, false)
+
+            holder = ViewHolder()
+            holder.thingName = view.findViewById(R.id.thingName) as TextView
+            holder.editButton = view.findViewById(R.id.editButton) as Button
+            holder.deleteButton = view.findViewById(R.id.deleteButton) as Button
+
+            view.tag = holder
+        }else{
+            view = convertView
+            holder = convertView.tag as ViewHolder
+        }
+
+        val thingName = holder.thingName
+
+        thingName.text = thing.name
+
+        return view
+    }
+
+    private class ViewHolder {
+        lateinit var thingName: TextView
+        lateinit var editButton: Button
+        lateinit var deleteButton: Button
     }
 }
