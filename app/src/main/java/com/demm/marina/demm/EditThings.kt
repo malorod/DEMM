ee73b4e1f9731330875948e55553d30656ef4cf4
malorod <maipsilon@gmail.com>
2018-08-14 20:01:38 +0200

    Actualización de la vista con actividad vacía

--- app/src/main/java/com/demm/marina/demm/EditThings.kt
+++ app/src/main/java/com/demm/marina/demm/EditThings.kt
@@ -1,21 +1,12 @@
 package com.demm.marina.demm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
-import kotlinx.android.synthetic.main.activity_edit_things.*
+import android.support.v7.app.AppCompatActivity
 
 class EditThings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_things)
-
-        val thingBundle = intent.extras
-        val name = thingBundle.getString("name").toString()
-        val placement = thingBundle.getString("placement").toString()
-
-        thingNameText.text = getString(R.string.thinNameText, name)
-        thingPlacementText.text = getString(R.string.thingPlacementText, placement)
     }
 }
