64c6f6e65cb094682eaeaa60a0ee7b6bb357ad76
Marina <diadora22*gh>
2018-08-18 06:56:15 +0200

    Vista añadir Lugar

---
+++ app/src/main/java/com/demm/marina/demm/ListAllPlacements.kt
@@ -0,0 +1,13 @@
+package com.demm.marina.demm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListAllPlacements : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_all_placements)
+
+    }
+}
