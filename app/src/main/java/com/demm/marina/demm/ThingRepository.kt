b5eabc3ec9294ac7a322a58d3112db710164aec0
malorod <maipsilon@gmail.com>
2018-08-27 17:07:39 +0200

    Eliminar funciones no utilizadas

--- app/src/main/java/com/demm/marina/demm/ThingRepository.kt
+++ app/src/main/java/com/demm/marina/demm/ThingRepository.kt
@@ -2,12 +2,8 @@ package com.demm.marina.demm
 
 interface ThingRepository {
     fun save(thing: Thing)
-    fun isEmpty(): Boolean
-    fun contains(thing: Thing): Boolean
-    fun getFirst(): Thing
     fun getThingList(thing: Thing): MutableList<Thing>
     fun getAllPlacement(thingName: String): MutableList<String>
     fun delete(thing: Thing)
     fun update(oldThing: Thing, newThing: Thing)
-    fun getAllPlacements(): MutableList<String>
 }
